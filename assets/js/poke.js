899eb00c6e972082bc12ce6974d6030a2516e383
Anthony Ahumada <locopala99@gmail.com>
2021-08-02 14:41:52 +0200

    more

--- assets/js/poke.js
+++ assets/js/poke.js
@@ -1,14 +1,11 @@
 const API="https://pokeapi.co/api/v2/pokemon?limit=20&offset=0";
 let datos ="";
 
-const ObtenerPokemon = (url,datos) =>{
+const ObtenerPokemon = (url) =>{
     return fetch(url)
     .then((response)=> response.json())
     .then((json)=>{
-       
-        datos = json.results[0].url;
-       
-      llenarDatos(json.results,datos),paginacion(json),console.log()
+      pokeData(json.results),paginacion(json),console.log()
      
 
       
@@ -21,21 +18,41 @@ const ObtenerPokemon = (url,datos) =>{
     
 }
 
+//otro fetch.
 
-const llenarDatos = (pokemon,datos)=>{
+const pokeData = (pokemon) =>{
     let html = "";
+    document.getElementById("datos_personajes").innerHTML="";
+    pokemon.forEach((pk)=>{
+        const URL = pk.url;
+        return fetch(URL)
+         .then((response)=>response.json())
+         .then((json)=>{
+            llenarDatos(json,html);
+         })
+         .catch((error)=>{
+            console.log("Error" , error);
+        });
+
+        
+    });
+}
+
+//dibujar las cards
+const llenarDatos = (pokemon,html)=>{
+    
 
     
-    pokemon.forEach((pk)=>{
+    console.log(pokemon)
 
-        html += `<div class="col mt-5 prueba"> <div class="card" style="width: 18rem;"> <img class="card-img-top" src="" alt="${pk.name}"> <div class="card-body"> <h5 class="card-title">${pk.name}</h5> <p class="card-text">${datos}</p> <p class="card-text"></p> </div> </div> </div>`
+        html += `<div class="col mt-5 prueba"> <div class="card" style="width: 18rem;"> <img class="card-img-top" src="${pokemon.sprites.other.dream_world.front_default}" alt="${pokemon.name}"> <div class="card-body"> <h5 class="card-title">${pokemon.name}</h5> <p class="card-text">${datos}</p> <p class="card-text"></p> </div> </div> </div>`
       
 
       
 
-    });
+    
 
-    document.getElementById("datos_personajes").innerHTML = html;
+    document.getElementById("datos_personajes").innerHTML += html;
     
 }
 
