811c785d3cac1abe526ff7d9a248ad11eca38b0f
Anthony Ahumada <locopala99@gmail.com>
2021-08-02 13:56:58 +0200

    more styles

--- assets/js/poke.js
+++ assets/js/poke.js
@@ -1,10 +1,18 @@
 const API="https://pokeapi.co/api/v2/pokemon?limit=20&offset=0";
+let datos ="";
 
-const ObtenerPokemon = (url) =>{
+const ObtenerPokemon = (url,datos) =>{
     return fetch(url)
     .then((response)=> response.json())
     .then((json)=>{
-      llenarDatos(json.results),paginacion(json),console.log(json) 
+       
+        datos = json.results[0].url;
+       
+      llenarDatos(json.results,datos),paginacion(json),console.log()
+     
+
+      
+     
     })
     //el error
     .catch((error)=>{
@@ -14,20 +22,14 @@ const ObtenerPokemon = (url) =>{
 }
 
 
-const llenarDatos = (pokemon)=>{
+const llenarDatos = (pokemon,datos)=>{
     let html = "";
+
+    
     pokemon.forEach((pk)=>{
 
-        html += '<div class="col mt-5 prueba">'
-        html += '<div class="card" style="width: 18rem;">';
-        html += `<img class="card-img-top" src="" alt="${pk.name}">`;
-        html += '<div class="card-body">';
-        html += `<h5 class="card-title">${pk.name}</h5>`
-        html += `<p class="card-text"></p>`
-        html += `<p class="card-text"></p>`
-        html += '</div>'
-        html += '</div>'
-        html += '</div>'
+        html += `<div class="col mt-5 prueba"> <div class="card" style="width: 18rem;"> <img class="card-img-top" src="" alt="${pk.name}"> <div class="card-body"> <h5 class="card-title">${pk.name}</h5> <p class="card-text">${datos}</p> <p class="card-text"></p> </div> </div> </div>`
+      
 
       
 
@@ -47,4 +49,4 @@ const paginacion = (boton)=>{
     document.getElementById("paginacion").innerHTML=html;
 }
 
-ObtenerPokemon(API);
+ObtenerPokemon(API,datos);
